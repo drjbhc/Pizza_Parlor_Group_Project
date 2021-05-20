7b422d308d93f853a32327ec7bdf4f73f44d2f5b
Tanya Engelhart <tkengelhart@gmail.com>
2021-05-20 11:35:06 -0500

    updated checkout

--- src/components/Checkout/Checkout.jsx
+++ src/components/Checkout/Checkout.jsx
@@ -1,6 +1,7 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { useHistory } from 'react-router-dom';
+import axios from 'axios';
 
 function Checkout() {
 
