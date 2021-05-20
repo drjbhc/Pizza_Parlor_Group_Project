7ca29656df25b51e230834b9f871ab0dd77101ff
Tanya Engelhart <tkengelhart@gmail.com>
2021-05-20 09:59:13 -0500

    initial reducers set up

--- src/index.js
+++ src/index.js
@@ -3,4 +3,31 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import logger from 'redux-logger';
+
+const pizzaReducer = (state = [], action) => {
+    return state;
+}
+const customerReducer = (state = [], action) => {
+    return state;
+}
+const cartReducer = (state = [], action) => {
+    return state;
+}
+
+
+const storeInstance = createStore(
+    combineReducers({
+        pizzaReducer,
+        customerReducer,
+        cartReducer,
+    }),
+    applyMiddleware(logger),
+);
+
+// Blaine's hack
+window.store = storeInstance;
+
+ReactDOM.render(<Provider store={storeInstance}><App /></Provider>, document.getElementById('root'));
